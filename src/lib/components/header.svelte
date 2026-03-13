4550fd9366d5b1eb1b5fcf3ed56398cfd9ff552b
Monty Pages <matmontypages@gmail.com>
2026-03-12 21:09:52 -0700

    added files and components

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,14 @@
+<script>
+    const headerImg = "src/lib/assets/logo-1.svg";
+</script>
+
+    <header>
+        <div class="container wide">
+            <div class="logo">
+                <img class="logo-img" src={headerImg} alt="Monty Pages">
+                <h1 class="logo-text">Monty Pages</h1>
+            </div>
+        </div>
+    </header>
+
+<style></style>
---
+++ src/lib/components/logo-button.svelte
@@ -0,0 +1,10 @@
+<script>
+    let { href } = $props();
+</script>
+
+    <button href={href}>
+            <img class="cta-img" src="assets/logo-2.svg" alt="Monty Pages">
+            <h2>Get Started</h2>
+    </button>
+
+<style></style>
