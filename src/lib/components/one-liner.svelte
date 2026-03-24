74d935fcd70f64e0e9f5373b49c5091ff4a58fc2
Monty Pages <matmontypages@gmail.com>
2026-03-24 00:45:49 -0700

    changed the language of the one-liner.

--- src/lib/components/one-liner.svelte
+++ src/lib/components/one-liner.svelte
@@ -5,7 +5,7 @@
 
     <div class="container">
         <h2 bind:this={heading}>Beautiful Custom Websites</h2>
-        <p bind:this={para}>You know you need an online presence, but you don't have the time or know-how to build your website. At Monty Pages we take the time to get to know you and your business so that we can build a custom website that beautifully represents you to your future clients. You'll feel great knowing that you have a website that accurately represents your organization.</p>
+        <p bind:this={para}>You know you need an online presence, but you building a website can be frustrating and time-consuming. At Monty Pages we take the time to get to know you and your business so that we can build a custom website that beautifully represents you to your future clients. You'll feel great knowing that you have a website that accurately represents your organization.</p>
     </div>
 
 <style></style>
