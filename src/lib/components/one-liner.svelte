f7b56f91b9770e951cd5379b6b2c8a63f14051a5
Monty Pages <matmontypages@gmail.com>
2026-04-06 21:15:04 -0700

    changed heading for one-liner

--- src/lib/components/one-liner.svelte
+++ src/lib/components/one-liner.svelte
@@ -4,7 +4,7 @@
 </script>
 
     <div class="container center">
-        <h2 bind:this={heading}>Beautiful Custom Websites</h2>
+        <h2 bind:this={heading}>Your voice. Your brand. Your site.</h2>
         <p bind:this={para}>You know you need an online presence, but you don't know who you can trust to deliver a website that fits you and your needs. At Monty Pages we take the time to get to know you and your business so that we can build a custom website that beautifully represents you to your future clients. You'll feel great knowing that you have a website that accurately represents your organization.</p>
     </div>
 
