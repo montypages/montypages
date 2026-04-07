7a0d08b8646e394984c62acee373014bfd06e7e2
Monty Pages <matmontypages@gmail.com>
2026-04-06 22:38:41 -0700

    importing logo image to header

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,11 +1,11 @@
 <script>
-    const headerImg = "src/lib/assets/logo-1.svg";
+    import logo from '$lib/assets/logo-1.svg';
 </script>
 
     <header data-section="0" class="section">
         <div class="container wide">
             <div class="logo">
-                <img class="logo-img" src={headerImg} alt="Monty Pages">
+                <img class="logo-img" src={logo} alt="Monty Pages">
                 <h1 class="logo-text">Monty Pages</h1>
                 <h2>Beautiful Custom Websites</h2>
             </div>
